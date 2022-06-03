527bdc4e7eeb7a2b745f468bfc053b51925d6f03
rrijn <aC9595496!>
2022-06-03 09:45:03 +0200

    Added installation project dependencies script

---
+++ installation/installProjectDependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#A Python library that enables the use of Jetson's GPIOs
+sudo -H python3 -m pip install inputs / pip3 install inputs 
+
+#Install driver for the MotorDriver, I2C and PowerSupply
+sudo -H python3 -m pip install Adafruit-MotorHAT / pip3 install
+
+#Install support of "Typed Attributes" enforcement framework
+sudo -H python3 -m pip install traitlets  / pip3 install traitlets
