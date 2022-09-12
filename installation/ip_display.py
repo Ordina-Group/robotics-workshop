b0353c8d063beb93111671d4f4ff78c86d576bdd
Louigi-Son <louigimar_3@hotmail.com>
2022-09-12 14:32:33 +0200

    Update ip_display.py
    - Change Font size to 14

--- installation/ip_display.py
+++ installation/ip_display.py
@@ -17,7 +17,7 @@ image = Image.new('1', (width, height))
 draw = ImageDraw.Draw(image)
 draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
-font = ImageFont.truetype("/usr/share/fonts/truetype/ubuntu/UbuntuMono-B.ttf", 16, encoding="unic")
+font = ImageFont.truetype("/usr/share/fonts/truetype/ubuntu/UbuntuMono-B.ttf", 14, encoding="unic")
 
 def extract_ip():
     st = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
