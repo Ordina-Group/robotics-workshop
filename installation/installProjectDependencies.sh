ff999c777362ce19c4577acba2dfc99328280adf
LRichardson <Louigimar.Richardson@koop.overheid.nl>
2023-02-15 15:09:59 +0100

    Added Pygame Instructions to installProjectDependencies.sh
    - command to install pygame

--- installation/installProjectDependencies.sh
+++ installation/installProjectDependencies.sh
@@ -9,5 +9,8 @@ sudo -H python3 -m pip install Adafruit-MotorHAT
 #Install support of "Typed Attributes" enforcement framework
 sudo -H python3 -m pip install traitlets
 
+#Install support of 'PyGame"
+sudo -H python3 -m pip install pygame
+
 #Add User to Input Group
 sudo gpasswd -a $USER input
